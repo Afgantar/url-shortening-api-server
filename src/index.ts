485027ffd85fb954de95821824940db530b4eadf
Afgantar <afgantarg@gmail.com>
2024-01-24 15:55:55 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.use(cors({
+  origin: [process.env.CLIENT_ORIGIN as string],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
+
+axios.defaults.withCredentials = true;
+
+app.post('/api/post', async (req, res) => {
+  try {
+    const response = await axios.post(process.env.CLEAN_URI_API as string, {
+      url: req.body.url,
+    });
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
