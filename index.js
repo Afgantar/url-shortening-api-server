a15707ec35255d841a3d04f1252ee11ca509b076
Afgantar <afgantarg@gmail.com>
2024-01-25 11:35:45 +0700

    Changed Error Message

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/api/post', async (req, res) => {
 
     res.status(response.status).json(response.data);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: `${error.response.data.message}` });
   }
 });
 
